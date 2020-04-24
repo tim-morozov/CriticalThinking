f64a84e4d9a573baa7e2a00198206c0f19db4386
tim-morozov <tim.morozov@gmail.com>
2020-04-24 09:39:18 -0500

    modifed While to break when user input is 42

--- CriticalThinking/WhileLoop.cs
+++ CriticalThinking/WhileLoop.cs
@@ -10,12 +10,11 @@ namespace CriticalThinking
     {
         public void While()
         {
-            Console.WriteLine("Where would you like to start? (must be less than 25)");
-            int counter = int.Parse(Console.ReadLine());
-            while(counter < 25)
+            int counter = 0;
+            while(counter != 42)
             {
-                Console.WriteLine(counter);
-                counter++;
+                Console.WriteLine("Please enter the number 42");
+                 counter = int.Parse(Console.ReadLine());
             }
         }
     }
